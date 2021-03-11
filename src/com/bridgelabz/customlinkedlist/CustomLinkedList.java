e1ea19bc399b84d4c038b8475b9d2f89ae5571b0
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-12 00:39:11 +0530

    Refactored to add Values at last

--- src/com/bridgelabz/customlinkedlist/CustomLinkedList.java
+++ src/com/bridgelabz/customlinkedlist/CustomLinkedList.java
@@ -1,19 +1,45 @@
 package com.bridgelabz.customlinkedlist;
 
-public class CustomLinkedList {
+public class CustomLinkedList<T> {
     int size;
-    Node start;
+    Node head;
 
     public CustomLinkedList() {
         this.size = 0;
-        this.start = null;
-    }
-
-    public boolean isEmpty() {
-        return (start == null) ? true : false;
+        this.head = null;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public void viewList() {
+        Node temp;
+        if (head == null) {
+            System.out.println("List is Empty");
+        }
+        else {
+            temp = head;
+            while (temp != null){
+                System.out.print(temp.key + " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void add(T value) {
+        Node node, temp;
+        node = new Node(value);
+        temp = head;
+        if (temp == null) {
+            head = node;
+        }
+        else {
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+        size++;
     }
 }
