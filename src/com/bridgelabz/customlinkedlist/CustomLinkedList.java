287e3bd2bbd1511fbedd5bad27c0db03299ba394
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-12 03:55:22 +0530

    Refactored to insert between the values at specified position

--- src/com/bridgelabz/customlinkedlist/CustomLinkedList.java
+++ src/com/bridgelabz/customlinkedlist/CustomLinkedList.java
@@ -49,4 +49,24 @@ public class CustomLinkedList<T> {
         head = node;
         size++;
     }
+
+    public void addAtPosition(T value, int position) {
+        if (position == 1) {
+            addAtFirst(value);
+        } else if (position == size + 1) {
+            add(value);
+        } else if (position > 1 && position <= size) {
+            Node node, temp;
+            node = new Node(value);
+            temp = head;
+            for (int i = 1; i < position; i++)
+                temp = temp.next;
+            node.next = temp.next;
+            temp.next = node;
+            size++;
+        } else {
+            System.out.println("Insertion is not possible at: " + position);
+
+        }
+    }
 }
